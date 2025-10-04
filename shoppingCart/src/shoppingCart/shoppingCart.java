3092dd3cd8e5c24a2501897bfa3d29a340271ee9
Simon Clifford <git@crookedmeadow.com>
2025-10-04 14:29:55 +0100

    bare bones class, method & test

---
+++ shoppingCart/src/shoppingCart/shoppingCart.java
@@ -0,0 +1,15 @@
+package shoppingCart;
+
+import java.math.BigDecimal;
+
+public class shoppingCart {
+	
+	private BigDecimal total = new BigDecimal(0.00);
+	
+	shoppingCart(){}
+	
+	public BigDecimal total_cost(String[] items) {
+		
+		return total;
+	}
+}
---
+++ shoppingCart/src/shoppingCart/shoppingCartTest.java
@@ -0,0 +1,14 @@
+package shoppingCart;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class shoppingCartTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
